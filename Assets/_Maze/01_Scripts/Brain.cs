9f58864fc8d43c42910b2821d87e40e91656ae9d
nobu06 <nobu.rs06@gmail.com>
2018-09-05 11:34:25 +0900

    add moving bots - has buggy fitness function, need to modify

---
+++ Assets/_Maze/01_Scripts/Brain.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+
+
+    public DNA dna;
+    private int dnaLength = 1;
+
+    private bool alive = true;
+
+    public GameObject startingPoint;
+    public float maxDistanceFromOrigin;
+    //public float maxDistanceToDestination;
+    //public GameObject destinationPoint;
+
+    private int numActions = 7;
+
+    private float translateModifier = 0.1f;
+
+    private void OnDestroy()
+    {
+        Destroy(gameObject);        // c?
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        // get the amount it should reflect by
+        if (col.gameObject.tag == "Wall")
+        {
+            int actionNum = dna.GetGene(0);
+
+            float reflectionNormal = CalculateRotationDirection(actionNum);
+
+            //// flip by chance
+            if (Random.Range(0, 2) < 1)
+            {
+                reflectionNormal *= -1;
+            }
+
+            Vector3 newDirection = Vector3.Reflect(transform.forward, col.contacts[0].normal);
+
+            // may be buggy
+//            newDirection = Quaternion.Euler(0f, reflectionNormal, 0f) * newDirection;       // https://answers.unity.com/questions/46770/rotate-a-vector3-direction.html
+
+
+            // rotate bot to a new direction
+            transform.rotation = Quaternion.LookRotation(newDirection);
+
+
+        }
+        else if (col.gameObject.tag == "Dead")
+        {
+            alive = false;
+
+            maxDistanceFromOrigin = 0;      // to not consider those that died as being fit
+        }
+    }
+
+
+    private float CalculateRotationDirection(int actionNum)
+    {
+        float rotationAngle = 0f;
+
+        switch(actionNum)
+        {
+            case 0:
+                rotationAngle = 10f;
+                break;
+            
+            case 1:
+                rotationAngle = 30f;
+                break;
+
+            case 2:
+                rotationAngle = 60f;
+                break;
+
+            case 3:
+                rotationAngle = 90f;
+                break;
+
+            case 4:
+                rotationAngle = 120f;
+                break;
+
+            case 5:
+                rotationAngle = 150f;
+                break;
+
+            case 6:
+                rotationAngle = 180f;
+                break;
+        }
+
+        //newRotation = new Vector3(0f, rotationAngle, 0f);
+
+        return rotationAngle;
+    }
+
+
+    public void Init()
+    {
+
+        dna = new DNA(dnaLength, numActions);
+
+        alive = true;
+
+        maxDistanceFromOrigin = 0f;
+
+        SetDestinationPoint();
+
+    }
+
+    private void SetDestinationPoint()
+    {
+        //destinationPoint = GameObject.FindWithTag("Destination");
+        startingPoint = GameObject.FindWithTag("StartingPoint");
+    }
+
+
+    private void Update()
+    {
+        if (!alive)
+            return;
+
+        // make it move forward
+        transform.Translate(Vector3.forward * translateModifier);
+
+        // calculate the max distance       // not a performant solution though?
+        float distanceFromOrigin = Vector3.Distance(transform.position, startingPoint.transform.position);
+        if (distanceFromOrigin > maxDistanceFromOrigin)
+        {
+            maxDistanceFromOrigin = distanceFromOrigin;
+        }
+    }
+
+
+    // will have dif num for each bot. Sth like the following. Perhaps have two.
+    // they will all keep moving forward by default. These num will control how
+    // they will react when they hit a wall.
+    // the DNA / Brain will store how much the bot rotates
+    // 0 - turn left by 30 degrees
+    // 1 - turn left by 60 degrees
+    // 2 - turn left by 90 degrees
+    // 3 - turn left by 120 degrees 
+    // 4
+    // 5
+    // 6
+
+
+
+    // you use NavMesh;
+    // dif genes represent how fast they move - I think
+
+
+
+}
+
+
+
+//Log:
+/*
+ Did:
+ - added Navmehs
+
+ - figure out how to make the capsule move around on the plane
+
+ - think about what to use as the gene traits.
+   (angle it rotates when it obunces the wall, the speed it moves, etc..)
+
+
+
+*/
---
+++ Assets/_Maze/01_Scripts/PopulationManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour {
+
+    public GameObject botPrefab;
+    public int populationSize = 2;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0f;
+    public float trialTime = 25;
+    int generation = 1;
+
+    public Text generationText;
+    public Text trialTimeText;
+    public Text populationText;
+
+	void Start () {
+        for (int i = 0; i < populationSize; i++)
+        {
+            Vector3 startingPos = new Vector3(transform.position.x, // + Random.Range(-1f, 1f),
+                                              transform.position.y,
+                                              transform.position.z); // + Random.Range(-1f, 1f));
+            GameObject bot = Instantiate(botPrefab, startingPos, Quaternion.identity);
+            bot.GetComponent<Brain>().Init();
+            population.Add(bot);
+        }
+	}
+	
+    private GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        Vector3 startingPos = new Vector3(transform.position.x, // + Random.Range(-1f, 1f),
+                                            transform.position.y,
+                                          transform.position.z); // + Random.Range(-1f, 1f));
+        GameObject offspring = Instantiate(botPrefab, startingPos, Quaternion.identity);
+        Brain brain = offspring.GetComponent<Brain>();
+
+        if (Random.Range(1, 100) == 1)
+        {
+            brain.Init();
+            brain.dna.Mutate();
+        }
+        else
+        {
+            brain.Init();
+            brain.dna.Combine(parent1.GetComponent<Brain>().dna, parent2.GetComponent<Brain>().dna);
+        }
+        return offspring;
+    }
+
+    private void BreedNewPopulation()
+    {
+        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<Brain>().maxDistanceFromOrigin).ToList();
+        population.Clear();
+
+        // breed upper half of sorted list
+        for (int i = (int)(sortedList.Count / 2f) - 1; i < sortedList.Count - 1; i++)
+        {
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i + 1], sortedList[i]));
+        }
+
+        // destroy all parents in previous population
+        for (int i = 0; i < sortedList.Count; i++)
+        {
+            Destroy(sortedList[i]);
+        }
+        generation++;
+    }
+
+	void Update () {
+        elapsed += Time.deltaTime;
+        if (elapsed >= trialTime)
+        {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+
+        UpdateUI();
+	}
+
+    private void UpdateUI()
+    {
+        generationText.text = "Generation: " + generation.ToString();
+        trialTimeText.text = "Trial time: " + ((int)elapsed).ToString();
+        populationText.text = "Population: " + population.Count.ToString();
+    }
+}
